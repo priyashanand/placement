9fb30358ef04655f37ab4a50a3255a307e47a026
priyashanand <tomriddlealbussirius@gmail.com>
2024-09-10 12:27:07 +0530

    frontend setup done

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1 className=' bg-yellow-600 text-lg'>This is the new website for college</h1>
+    </div>
+  )
+}
+
+export default App
